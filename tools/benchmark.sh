ad8fc55ed3b8183a5b3585b4066049adc95d1fc6
ilretho <ilretho@proton.me>
2026-05-30 00:26:34 +0200

    Rework tools/benchmark.sh to properly handle arguments using getopts; Make it able to call external tools to compile more data about files

--- tools/benchmark.sh
+++ tools/benchmark.sh
@@ -1,9 +1,130 @@
 #!/bin/bash
 
-if [ $# -lt 7 ]; then
-  echo "Usage: $0 <DIR> <NAME-SCHEME> <DOWNSAMPLE-DEGREE> <SCALE-LOW> <SCALE-HIGH> <CPULIMIT> <OUT-FILE>"
-  echo "  Example: $0 demo \"*.jpg\" 2 0 999999 30 demo/statistics.json"
-  exit
+printhelp() {
+  echo ""
+  echo "Usage: $0 <options>"
+  echo ""
+  echo "Required options:"
+  echo "  -i <in-dir>: Directory containing all files to be processed."
+  echo "  -n <name-scheme>: Shell pattern of input files to match. See documentation for find and its -name option."
+  echo "  -o <out-file>: Path to the file with the generated data."
+  echo ""
+  echo "Optional options:"
+  echo "  -l <scale-lower> [default: 0]"
+  echo "  -h <scale-higher> [default: 999999]"
+  echo "  -c <cpulimit> [default: 300]"
+  echo "  -d <downsample> [default: 0]"
+  echo "  -r <number-of-runs> [default: 1] NOT YET IMPLEMENTED"
+  echo "  -e <command-string>: Allows to call external tools and add their data to the generated file."
+  echo "    <command-string> must consist of two parts separated by a colon (:)."
+  echo "    The first part is a command to be run, while the second part is the corresponding field name."
+  echo "    For each input file found, the given command will be run with the file's path as its argument,"
+  echo "      and the command's output will be added to the data generated per file under its fieldname."
+  echo "    The time this command takes will not be included in the solving time."
+  echo "    Example: ... -e \"./laplacian.sh:laplace-blur\""
+  echo "    This option can be present multiple times."
+  echo ""
+  echo "Example:"
+  echo "  $0 -i demo/ -n \"*.jpg\" -o stats.json"
+  exit 1
+}
+
+intreg="^[0-9]+$"
+numreg="^([0-9]+(\.[0-9]+)?|\.[0-9]+)$"
+
+execind=0
+
+while getopts "i:n:o:l:h:c:d:r:e:" opt; do
+  case $opt in
+    i)
+      if ! [ -d "$OPTARG" ]; then
+        echo "ERROR: Input directory \"$OPTARG\" is not a directory."; printhelp
+      fi
+      indir="$OPTARG"
+      ;;
+    n)
+      namescheme="$OPTARG"
+      ;;
+    o)
+      if [ -e "$OPTARG" ]; then
+        echo "ERROR: Output file \"$OPTARG\" already exists."; printhelp
+      fi
+      outfile="$OPTARG"
+      ;;
+    l)
+      if ! [[ "$OPTARG" =~ $numreg ]] ; then
+         echo "ERROR: Lower scale bound (-l) needs to be a number."; printhelp
+      fi
+      scalelow="$OPTARG"
+      ;;
+    h)
+      if ! [[ "$OPTARG" =~ $numreg ]] ; then
+        echo "ERROR: Upper scale bound (-h) needs to be a number."; printhelp
+      fi
+      scalehigh="$OPTARG"
+      ;;
+    c)
+      if ! [[ "$OPTARG" =~ $intreg ]] ; then
+        echo "ERROR: CPU limit (-c) needs to be an integer."; printhelp
+      fi
+      cpulimit="$OPTARG"
+      ;;
+    d)
+      if ! [[ "$OPTARG" =~ $intreg ]] ; then
+        echo "ERROR: Downsampling degree (-d) needs to be an integer."; printhelp
+      fi
+      downsample="$OPTARG"
+      ;;
+    r)
+      if ! [[ "$OPTARG" =~ $intreg ]] ; then
+        echo "ERROR: Amount of repeats (-r) needs to be an integer."; printhelp
+      fi
+      repeats="$OPTARG"
+      ;;
+    e)
+      if [[ "$OPTARG" != *":"* ]]; then
+        echo "ERROR: -e requires format \"command:fieldname\" (provided: \"$OPTARG\")."
+        printhelp
+      fi
+      cmds[execind]="${OPTARG%:*}"
+      fieldnames[execind]="${OPTARG##*:}"
+      execind=$((execind+1))
+      ;;
+    \?)
+      printhelp
+      ;;
+  esac
+done
+
+if [ -z "$indir" ]; then
+  echo "Input directory (-i) is required."; printhelp
+fi
+
+if [ -z "$namescheme" ]; then
+  echo "File name scheme (-n) is required."; printhelp
+fi
+
+if [ -z "$outfile" ]; then
+  echo "Output file location (-o) is required."; printhelp
+fi
+
+if [ -n "$scalelow" ] && [ -n "$scalehigh" ]; then
+  if (( $(echo "$scalelow > $scalehigh" | bc -l) )); then
+    echo "ERROR: Upper scale bound (-h) needs to be greater than lower scale bound (-l)."
+    printhelp
+  fi
+fi
+
+if [ -z "$scalehigh" ]; then scalehigh=999999; fi
+if [ -z "$scalelow" ]; then scalelow=0; fi
+if [ -z "$cpulimit" ]; then cpulimit=300; fi
+if [ -z "$downsample" ]; then downsample=0; fi
+if [ -z "$repeats" ]; then repeats=1; fi
+
+if [ "$EUID" -eq 0 ] && [ -z "$AN_ALLOW_ROOT" ]; then
+  echo "Script is run at root, but AN_ALLOW_ROOT is not set."
+  echo "Please set AN_ALLOW_ROOT to run this script as root."
+  exit 1
 fi
 
 # Returns value of NDISTRACT field in .match file
@@ -31,31 +152,29 @@ bright_unrecognized() {
   echo $((expected_n - corr_field_n))
 }
 
-statsfile="$7"
-if [ -e "$statsfile" ]; then
-  echo "$statsfile already exists."
-  exit
-fi
-
 mydir="$(dirname "$0")"
 cleanscript="$mydir/clean.sh"
-
-downsample="$3"
-scalelow="$4"
-scalehigh="$5"
-cpulimit="$6"
 
 solved=0
 notsolved=0
 timetaken=0
 shopt -s lastpipe
 
-printf "{\n  \"downsample\": %s,\n  \"scale-low\": %s,\n  \"scale-high\": %s,\n  \"cpulimit\": %s"\
-  "$downsample" "$scalelow" "$scalehigh" "$cpulimit" >> "$statsfile"
-printf ",\n  \"images\": [" >> "$statsfile"
+printf "{\n  \"downsample\": %s,\n  \"scale-low\": %s,\n  \"scale-high\": %s,\n  \"cpulimit\": %s,\n  \"repeats\": %s"\
+  "$downsample" "$scalelow" "$scalehigh" "$cpulimit" "$repeats" >> "$outfile"
+printf ",\n  \"images\": [" >> "$outfile"
 
-find "$1" -mindepth 1 -maxdepth 1 -type f -name "$2" | sort | while read -r file; do
+inputfiles=$(find "$indir" -mindepth 1 -maxdepth 1 -type f -name "$namescheme" | sort)
+while read -r file; do
   echo "Solving $file..."
+
+  printf "\n    {" >> "$outfile"
+
+  # Execute all commands from -e
+  for i in $(seq 0 $((execind-1))); do
+    execout="$(${cmds[i]} "$file")"
+    printf "\n      \"%s\": \"%s\"," "${fieldnames[i]}" "$execout" >> "$outfile"
+  done
 
   # Gather data
   t0=$( echo $EPOCHREALTIME | tr -dc "0-9")
@@ -66,9 +185,6 @@ find "$1" -mindepth 1 -maxdepth 1 -type f -name "$2" | sort | while read -r file
 
   noext="${file%.*}"
   numsources="$(listhead "$noext.axy" | grep "NAXIS2" | xargs | cut -d " " -f3)"
-
-  # Write data
-  printf "\n    {" >> "$statsfile"
 
   if [ -f "$noext.solved" ]; then
     echo "Solved $file in $td ms."
@@ -87,7 +203,7 @@ find "$1" -mindepth 1 -maxdepth 1 -type f -name "$2" | sort | while read -r file
       printf "\n      \"ncorr\": %s," "$numcorrs"
       printf "\n      \"ndistract\": %s," "$ndistract"
       printf "\n      \"nbrighter\": %s" "$numbrightunrecognized"
-    } >> "$statsfile"
+    } >> "$outfile"
   else
     echo "Could not solve $file."
     notsolved=$((notsolved+1))
@@ -95,16 +211,16 @@ find "$1" -mindepth 1 -maxdepth 1 -type f -name "$2" | sort | while read -r file
       printf "\n      \"file\": \"%s\"," "$(basename "$file")"
       printf "\n      \"solved\": false,"
       printf "\n      \"nsrc\": %s" "$numsources"
-    } >> "$statsfile"
+    } >> "$outfile"
   fi
 
-  printf "\n    }," >> "$statsfile"
+  printf "\n    }," >> "$outfile"
 
   echo "Cleaning up..."
-  $cleanscript "$1" "$(basename "$noext")" 1>/dev/null
+  $cleanscript "$indir" "$(basename "$noext")" 1>/dev/null
 
-done
+done <<< "$inputfiles"
 
-truncate -s-1 "$statsfile" # Remove the last , in the array
+truncate -s-1 "$outfile" # Remove the last , in the array
 printf "\n  ],\n  \"solved\": %s,\n  \"avgtime\": %s,\n  \"notsolved\": %s\n}\n"\
-  "$solved" "$((timetaken / solved))" "$notsolved" >> "$statsfile"
+  "$solved" "$((timetaken / solved))" "$notsolved" >> "$outfile"
